7b6238283a4d84365cea94518ff5fa2fc73b5eb1
Pham Hoang Son <sonpham3798@gmail.com>
2019-11-28 23:44:11 +0700

    MainActivity Layout with Fragment and DashboardFragment

---
+++ app/src/main/java/com/duatson/studentapp/DashboardFragment.java
@@ -0,0 +1,23 @@
+package com.duatson.studentapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
+
+        return view;
+    }
+}
