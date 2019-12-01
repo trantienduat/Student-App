8dafb25efe8d3968f16871159d3abd6556b4fc23
Pham Hoang Son <sonpham3798@gmail.com>
2019-12-01 13:28:54 +0700

    ServiceDetail imp CustomListItem

--- app/src/main/java/com/duatson/studentapp/ServiceDetailFragment.java
+++ app/src/main/java/com/duatson/studentapp/ServiceDetailFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class ServiceDetailFragment extends Fragment {
 
+
+    ListView lvContact;
 
     public ServiceDetailFragment() {
         // Required empty public constructor
@@ -25,7 +28,22 @@ public class ServiceDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service_detail, container, false);
+        //return inflater.inflate(R.layout.fragment_service_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_service_detail, container,false);
+
+        setLvContact(view);
+
+        return view;
+    }
+
+    private void setLvContact(View view) {
+        String[] name = {"Apple", "Banana", "Orange"};
+        String[] info = {"Great", "Awesome", "Perfect"};
+        Integer[] img = {R.drawable.ic_arrow_back, R.drawable.ic_menu, R.drawable.ic_send};
+
+        CustomListAdapter customList = new CustomListAdapter(getActivity(), name, info, img);
+        lvContact = view.findViewById(R.id.lvContact);
+        lvContact.setAdapter(customList);
     }
 
 }
