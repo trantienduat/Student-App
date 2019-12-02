e2f93d9dd619d27cbbdb0ae5b87caacb934439d4
trinnguyen888 <trinnguyen888@gmail.com>
2019-12-03 00:32:25 +0700

    Update profile screen

--- app/src/main/java/com/duatson/studentapp/ServiceListActivity.java
+++ app/src/main/java/com/duatson/studentapp/ServiceListActivity.java
@@ -3,12 +3,25 @@ package com.duatson.studentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ServiceListActivity extends AppCompatActivity {
+
+    private ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_list);
+
+        imgBack = findViewById(R.id.imgBack);
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
