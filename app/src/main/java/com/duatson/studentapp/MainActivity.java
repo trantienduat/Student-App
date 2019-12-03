814ed7268078f604e49db961691721c76c58f0bb
huynhkimtri <huynhkimtri.fuhcm@gmail.com>
2019-12-03 15:51:44 +0700

    update branch

--- app/src/main/java/com/duatson/studentapp/MainActivity.java
+++ app/src/main/java/com/duatson/studentapp/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
